85581118de01a8072d924a6a58e371d1ae0d01b4
Turubilli Sairam <saiwweram@gmail.com>
2026-01-06 13:19:47 +0530

    update header

--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -5,25 +5,59 @@ import Countdown from "./Countdown";
 export default function HeaderInfo() {
 
   const [data, setData] = useState(null);
+  const [now, setNow] = useState(new Date());
 
+  // 🔹 Fetch backend data (NO NEED every second)
   useEffect(() => {
     const fetch = () =>
       API.get("/header").then(res => setData(res.data));
 
     fetch();
-    const t = setInterval(fetch, 1000);
-
+    const t = setInterval(fetch, 10000); // every 10 seconds
     return () => clearInterval(t);
+  }, []);
+
+  // 🔹 LIVE CLOCK (every second)
+  useEffect(() => {
+    const clock = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+    return () => clearInterval(clock);
   }, []);
 
   if (!data) return null;
 
+  const liveTime = now.toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+  });
+
+  const liveDate = now.toLocaleDateString("en-IN");
+
   return (
     <div className="header-box">
-      <div>visitors<br /><b>{data.visitors}</b></div>
-      <div>next draw time<br /><b>{data.nextDrawTime}</b></div>
-      <div>date<br /><b>{data.date}</b></div>
-      <div>time<br /><b>{data.time}</b></div>
+      <div>
+        visitors<br />
+        <b>{data.visitors}</b>
+      </div>
+
+      <div>
+        next draw time<br />
+        <b>{data.nextDrawTime}</b>
+      </div>
+
+      <div>
+        date<br />
+        <b>{liveDate}</b>
+      </div>
+
+      <div>
+        time<br />
+        <b>{liveTime}</b>
+      </div>
+
       <div>
         time to draw<br />
         <b><Countdown seconds={data.timeToDraw} /></b>
